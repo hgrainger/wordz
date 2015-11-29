59afb969296904b4213d13cd690afc8623e977f7
H. Grainger <cody@upnorthagency.com>
2015-11-29 10:27:36 -0600

    pep8 fixes

--- get_content.py
+++ get_content.py
@@ -5,19 +5,20 @@ import sqlite3
 
 
 conn = sqlite3.connect('db.sqlite')
+# alchemy apikey
+apikey = 'apikey'
+requrltit = 'http://gateway-a.watsonplatform.net/calls/url/URLGetTitle?'
+requrltex = 'http://gateway-a.watsonplatform.net/calls/url/URLGetText?'
 
 
 def get_article(url):
-    apikey = 'alchemy api key'
-    requrltit = 'http://gateway-a.watsonplatform.net/calls/url/URLGetTitle?'
-    requrltex = 'http://gateway-a.watsonplatform.net/calls/url/URLGetText?'
     callapi_title = requrltit + apikey + url + '&outputMode=json'
     callapi_text = requrltex + apikey + url + '&outputMode=json'
     ans1 = json.load(urllib2.urlopen(callapi_title))
     ans2 = json.load(urllib2.urlopen(callapi_text))
     to_spin_title = ans1['title']
     to_spin_text = ans2['text']
-    connect(to_spin_title, to_spin_text)
+    return connect(to_spin_title, to_spin_text)
 
 
 def connect(new_title, new_article):
